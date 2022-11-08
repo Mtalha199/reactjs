b694418c30d8a1edcbc90191f41ad5254c8337e8
MTalha <talhatarar19@gmail.com>
2022-11-08 16:24:02 +0500

    jsx-attributes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-//REACT Template Literal
+//REACT JSX Attributes
+//  JSX attributes is look like an html attributes it work also as html 
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-let my_var="talha";
-let fname="Arshad Mehmood";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
       <>
-      {/* if we want to write all in one line we use many method but now we do in Template literal */}
-      <h1>{`My name is ${my_var} My father name is ${fname}`}</h1>
-      <h2>My Age is {20+2}</h2>
-      <h3>My lucky num is {Math.random()}</h3>
+      
+      {/* contentEditable is an attribute of JSX  */}
+      <h1 contentEditable> MY NAME IS TALHA</h1>
+      <img src='https://picsum.photos/200/300' alt='random pics' />
+
+      {/* if i want to go another website by click on img */}
+      <a href='https://www.w3schools.com/html/html_attributes.asp' >
+            <img src='https://picsum.photos/250/300' />
+      </a>
+
       </>
 );
 
