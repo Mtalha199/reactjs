d7b5ce072e4ec7d9ef7e05354ffd2e440cadd2af
MTalha <talhatarar19@gmail.com>
2022-11-08 15:07:35 +0500

    template-literal

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-//REACT Expression
+//REACT Template Literal
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//we can access a variable by {}
 let my_var="talha";
 let fname="Arshad Mehmood";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
       <>
-      <h1>My name is {my_var}</h1>
-      <p>My father name is {fname}</p>
+      //if we want to write all in one line we use many method but now we do in Template literal
+      <h1>{`My name is ${my_var} My father name is ${fname}`}</h1>
+      
       <h2>My Age is {20+2}</h2>
       <h3>My lucky num is {Math.random()}</h3>
       </>
-      //we cant use in statment e.g loop,condition etc 
 );
 
 
