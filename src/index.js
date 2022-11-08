cc9d8ede61fe4957c4f2d1a08c23ff9551fe87e6
MTalha <talhatarar19@gmail.com>
2022-11-08 14:47:29 +0500

    react-expression

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+//REACT Expression
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+//we can access a variable by {}
+let my_var="talha";
+let fname="Arshad Mehmood";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-      //React fragment is use to write diffrent JSX 
-      //React fragment dont use extra memory
-      // <React.Fragment>
-      //you can also use it without react fragment word by using only empty 
       <>
-      <h1>Hello this is my first react web</h1>
-      <p>One day In Sha ALLAH become a developer</p>
-      <h2>coming soon</h2>
-      </>,
+      <h1>My name is {my_var}</h1>
+      <p>My father name is {fname}</p>
+      <h2>My Age is {20+2}</h2>
+      <h3>My lucky num is {Math.random()}</h3>
+      </>
+      //we cant use in statment e.g loop,condition etc 
 );
+
 
