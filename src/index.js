c1ca406d908dc6b9cb2a790a2c42545a9e930eac
MTalha <talhatarar19@gmail.com>
2022-11-08 15:07:59 +0500

    template-literal

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ let fname="Arshad Mehmood";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
       <>
-      //if we want to write all in one line we use many method but now we do in Template literal
+      {/* if we want to write all in one line we use many method but now we do in Template literal */}
       <h1>{`My name is ${my_var} My father name is ${fname}`}</h1>
-      
       <h2>My Age is {20+2}</h2>
       <h3>My lucky num is {Math.random()}</h3>
       </>
